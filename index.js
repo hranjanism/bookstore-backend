983fafb037b04ba1fc6f44dc26068199b6b202ec
hranjanism <himanshu.21611@gmail.com>
2025-11-15 18:15:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bookRoutes from "./routes/books.js";
+import seed from "./seed.js";
 
 dotenv.config();
 const app = express();
@@ -10,18 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/books", bookRoutes);
-
-// Database Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("DB error:", err));
 
 app.get("/", (req, res) => {
   res.send("Bookstore API is running...");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    seed();
+  })
+  .catch((err) => console.log("DB error:", err.message));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
