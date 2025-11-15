afd41838339bee2c8f1ee92bf2dc6e1579b1c0d4
hranjanism <himanshu.21611@gmail.com>
2025-11-15 18:18:19 +0530

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -3,17 +3,36 @@ import Book from "../models/Book.js";
 
 const router = express.Router();
 
-// Get all books
+// GET all books
 router.get("/", async (req, res) => {
-  const books = await Book.find();
-  res.json(books);
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-// Add a book
+// GET one book
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ msg: "Book not found" });
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST new book
 router.post("/", async (req, res) => {
-  const book = new Book(req.body);
-  await book.save();
-  res.json({ message: "Book added!", book });
+  try {
+    const book = new Book(req.body);
+    await book.save();
+    res.status(201).json({ message: "Book added!", book });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 export default router;
